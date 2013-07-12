928b4e73961d1e7ac94935c4d9501e2e6d58afea
tcorral <amischol@gmail.com>
2013-07-12 12:24:03 +0200

    Update Hydra.js to version Update Profiler to show more info

--- Profiler/src/Profiler.js
+++ Profiler/src/Profiler.js
@@ -1,37 +1,67 @@
-(function(win, Hydra){
-	'use strict';
-  var Profiler = function(){};
+(function ( win, Hydra )
+{
+  'use strict';
+  var Profiler = function ()
+  {
+  };
   Profiler.prototype = {
-    subscribersCount: function(){
+    subscribersCount: function ( bShowEvents )
+    {
       var nSubscribers = 0,
         oCurrentChannel,
         sChannel,
         sEvent,
+        oEvents = {},
+        nLenSubscribers,
         oChannels = Hydra.getCopyChannels();
-      for(sChannel in oChannels){
-        if(oChannels.hasOwnProperty(sChannel)){
+
+      for ( sChannel in oChannels )
+      {
+        if ( oChannels.hasOwnProperty( sChannel ) )
+        {
           oCurrentChannel = oChannels[sChannel];
-          for(sEvent in oCurrentChannel){
-            if(oCurrentChannel.hasOwnProperty(sEvent)){
-              nSubscribers += oCurrentChannel[sEvent].length;
+          for ( sEvent in oCurrentChannel )
+          {
+            if ( oCurrentChannel.hasOwnProperty( sEvent ) )
+            {
+              nLenSubscribers = oCurrentChannel[sEvent].length;
+              oEvents[sChannel + '|' + sEvent] = nLenSubscribers;
+              nSubscribers += nLenSubscribers;
             }
           }
         }
       }
+      if ( bShowEvents )
+      {
+        oEvents.TOTAL = nSubscribers;
+        return oEvents;
+      }
       return nSubscribers;
     },
-    modulesCount: function(){
+    modulesCount: function ( bShowModules )
+    {
       var nInstances = 0,
         sModule,
-        oModules = Hydra.getCopyModules();
-      for(sModule in oModules){
-        if(oModules.hasOwnProperty(sModule)){
-          nInstances += Object.keys(oModules[sModule].instances ).length;
+        oModules = Hydra.getCopyModules(),
+        nLenModules,
+        oModulesToShow = {};
+      for ( sModule in oModules )
+      {
+        if ( oModules.hasOwnProperty( sModule ) )
+        {
+          nLenModules = Object.keys( oModules[sModule].instances ).length;
+          oModulesToShow[sModule] = nLenModules;
+          nInstances += nLenModules;
         }
+      }
+      if ( bShowModules )
+      {
+        oModules.TOTAL = nInstances;
+        return oModules;
       }
       return nInstances;
     }
   };
 
-  Hydra.extend("profiler", new Profiler());
-}(window, Hydra));
+  Hydra.extend( "profiler", new Profiler() );
+}( window, Hydra ));
--- Profiler/test/ProfilerTest.js
+++ Profiler/test/ProfilerTest.js
@@ -1,83 +1,175 @@
-(function ( win, doc, Hydra ) {
-	'use strict';
-	TestCase('Profiler subscribersCount', sinon.testCase({
-    setUp: function(){
-      Hydra.module.remove('test');
+(function ( win, doc, Hydra )
+{
+  'use strict';
+  TestCase( 'Profiler subscribersCount', sinon.testCase( {
+    setUp: function ()
+    {
+      Hydra.module.remove( 'test' );
+      Hydra.bus.reset();
     },
-    tearDown: function(){},
-    'test should check that subscribersCount returns 0 if there are no modules started even if they are registered': function(){
-      Hydra.module.register('test', function(){
+    tearDown: function ()
+    {
+    },
+    'test should check that subscribersCount returns 0 if there are no modules started even if they are registered': function ()
+    {
+      Hydra.module.register( 'test', function ()
+      {
         return {
           events: {
             global: {
-              'test': function(){}
+              'test': function ()
+              {
+              }
             }
           }
         }
-      });
-      assertEquals(0, Hydra.profiler.subscribersCount());
+      } );
+      assertEquals( 0, Hydra.profiler.subscribersCount() );
     },
-    'test should check that subscribersCount returns 1 if a module is started with the current signature': function(){
-      Hydra.module.register('test', function(){
+    'test should check that subscribersCount returns 1 if a module is started with the current signature': function ()
+    {
+      Hydra.module.register( 'test', function ()
+      {
         return {
           events: {
             global: {
-              'test': function(){}
+              'test': function ()
+              {
+              }
             }
           }
         }
-      });
-      Hydra.module.start('test');
-      assertEquals(1, Hydra.profiler.subscribersCount());
+      } );
+      Hydra.module.start( 'test' );
+      assertEquals( 1, Hydra.profiler.subscribersCount() );
+    },
+    'test should check that if we call subscribersCount with a true param it will return an object with all the events and the number of subscribers': function ()
+    {
+      var oSubscribers;
+      Hydra.module.register( 'test', function ()
+      {
+        return {
+          events: {
+            global: {
+              'test': function ()
+              {
+              }
+            }
+          }
+        }
+      } );
+      Hydra.module.start( 'test' );
+      oSubscribers = Hydra.profiler.subscribersCount( true );
+      assertObject( oSubscribers );
+      assertEquals( 1, oSubscribers.TOTAL );
+      assertEquals( 1, oSubscribers['global|test'] );
     }
-  }));
+  } ) );
 
-  TestCase('Profiler modulesCount', sinon.testCase({
-    setUp: function(){
-      Hydra.module.remove('test');
+  TestCase( 'Profiler modulesCount', sinon.testCase( {
+    setUp: function ()
+    {
+      Hydra.module.remove( 'test' );
     },
-    tearDown: function(){},
-    'test should check that modulesCount returns 0 if there are no modules registered': function(){
-      assertEquals(0, Hydra.profiler.modulesCount());
+    tearDown: function ()
+    {
     },
-    'test should check that modulesCount returns 0 if there are modules registered but no started': function(){
-      Hydra.module.register('test', function(){
+    'test should check that modulesCount returns 0 if there are no modules registered': function ()
+    {
+      assertEquals( 0, Hydra.profiler.modulesCount() );
+    },
+    'test should check that modulesCount returns 0 if there are modules registered but no started': function ()
+    {
+      Hydra.module.register( 'test', function ()
+      {
         return {
           events: {
             global: {
-              'test': function(){}
+              'test': function ()
+              {
+              }
             }
           }
         }
-      });
-      assertEquals(0, Hydra.profiler.modulesCount());
+      } );
+      assertEquals( 0, Hydra.profiler.modulesCount() );
     },
-    'test should check that modulesCount returns 1 if a module is registered and started': function(){
-      Hydra.module.register('test', function(){
+    'test should check that modulesCount returns 1 if a module is registered and started': function ()
+    {
+      Hydra.module.register( 'test', function ()
+      {
         return {
           events: {
             global: {
-              'test': function(){}
+              'test': function ()
+              {
+              }
             }
           }
         }
-      });
-      Hydra.module.start('test');
-      assertEquals(1, Hydra.profiler.modulesCount());
+      } );
+      Hydra.module.start( 'test' );
+      assertEquals( 1, Hydra.profiler.modulesCount() );
     },
-    'test should check that modulesCount returns 2 if a module is registered but is started two times': function(){
-      Hydra.module.register('test', function(){
+    'test should check that modulesCount returns 2 if a module is registered but is started two times': function ()
+    {
+      Hydra.module.register( 'test', function ()
+      {
         return {
           events: {
             global: {
-              'test': function(){}
+              'test': function ()
+              {
+              }
             }
           }
         }
-      });
-      Hydra.module.start('test');
-      Hydra.module.start('test');
-      assertEquals(2, Hydra.profiler.modulesCount());
+      } );
+      Hydra.module.start( 'test' );
+      Hydra.module.start( 'test' );
+      assertEquals( 2, Hydra.profiler.modulesCount() );
+    },
+    'test should check that if we call modulesCount with a true param it will return an object with all the modules and the number of modules instanciated': function ()
+    {
+      var oModules;
+      Hydra.module.register( 'test', function ()
+      {
+        return {
+          events: {
+            global: {
+              'test': function ()
+              {
+              }
+            }
+          }
+        }
+      } );
+      Hydra.module.start( 'test' );
+      oModules = Hydra.profiler.modulesCount( true );
+      assertObject( oModules );
+      assertEquals( 1, oModules.TOTAL );
+      assertEquals( 1, Object.keys( oModules.test.instances ).length );
+    },
+    'test should check that if we call modulesCount with a true param it will return an object with all the modules and the number of modules instanciated called two times': function ()
+    {
+      var oModules;
+      Hydra.module.register( 'test', function ()
+      {
+        return {
+          events: {
+            global: {
+              'test': function ()
+              {
+              }
+            }
+          }
+        }
+      } );
+      Hydra.module.start( 'test' );
+      Hydra.module.start( 'test' );
+      oModules = Hydra.profiler.modulesCount( true );
+      assertEquals( 2, oModules.TOTAL );
+      assertEquals( 2, Object.keys( oModules.test.instances ).length );
     }
-  }));
+  } ) );
 }( window, document, Hydra ));
